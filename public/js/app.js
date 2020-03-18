7963caf9abc3d545813c5e54f6fdb2fa429aa751
brandonbj <brandonlaraaguilar@gmail.com>
2020-03-17 21:19:04 -0600

    cambios estilo

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\Prefectura\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\Prefectura\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Prefectura\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Prefectura\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
